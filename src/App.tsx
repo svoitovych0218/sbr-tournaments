ec30400f188b88372eff5c3a3b88c4a9264bf949
Serhii Voitovych <vsv0218@gmail.com>
2024-02-01 15:11:18 +0200

    init 2

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,11 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { Chip } from '@mui/material';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import dayjs, { Dayjs } from 'dayjs';
 
 const urls = {
   dev: 'https://dev-api.battle-royale.site/api/leaderboard/tournaments',
@@ -26,36 +31,49 @@ const urls = {
 function App() {
   const [data, setData] = useState<any>(undefined);
   const [env, setEnv] = useState<string>('1');
+  const [dateTime, setDateTime] = useState<Dayjs | null>(dayjs(new Date().toISOString()))
   useEffect(() => {
     (async () => {
-      const url = env === '1' ? urls.dev : env === '2' ? urls.stage : urls.production;
+      let url = env === '1' ? urls.dev : env === '2' ? urls.stage : urls.production;
+      if (dateTime) {
+        url+=`?from=${dateTime.toISOString()}`;
+      }
       const response = await axios.get(url);
       setData(response.data);
     })();
-  }, [env]);
+  }, [env, dateTime]);
 
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          <Paper sx={{width: 100, marginLeft: 'auto'}}>
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">ENV</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={env}
-                label="Env"
-                onChange={(e: any) => {
-                  setEnv(e.target.value.toString());
-                }}
-              >
-                <MenuItem value={1}>Dev</MenuItem>
-                <MenuItem value={2}>Playtest</MenuItem>
-                <MenuItem value={3}>Prod</MenuItem>
-              </Select>
-            </FormControl>
-          </Paper>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Paper sx={{ width: 300, marginRight: 'auto' }}>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DemoContainer components={['DateTimePicker']}>
+                  <DateTimePicker value={dateTime} onChange={(newValue) => setDateTime(newValue)} label="From" />
+                </DemoContainer>
+              </LocalizationProvider>
+            </Paper>
+            <Paper sx={{ width: 100, marginLeft: 'auto' }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">ENV</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={env}
+                  label="Env"
+                  onChange={(e: any) => {
+                    setEnv(e.target.value.toString());
+                  }}
+                >
+                  <MenuItem value={1}>Dev</MenuItem>
+                  <MenuItem value={2}>Playtest</MenuItem>
+                  <MenuItem value={3}>Prod</MenuItem>
+                </Select>
+              </FormControl>
+            </Paper>
+          </div>
           {data && <TableContainer sx={{ maxWidth: 1200 }} component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
               <TableHead>
@@ -78,12 +96,12 @@ function App() {
                       {i + 1}
                     </TableCell>
                     <TableCell component="th" scope="row">
-                      {row.playedAt}
+                      {new Date(row.playedAt).toLocaleString()}
                     </TableCell>
                     <TableCell align="right">{row.teamStats[0]?.totalPoints.toFixed()}</TableCell>
                     <TableCell align="right">{row.teamStats[1]?.totalPoints.toFixed()}</TableCell>
-                    <TableCell align="right">{row.teamStats[0]?.userNames.map((s:string) => (<Chip label={s} variant="outlined" />))}</TableCell>
-                    <TableCell align="right">{row.teamStats[1]?.userNames.map((s:string) => (<Chip label={s} variant="outlined" />))}</TableCell>
+                    <TableCell align="right">{row.teamStats[0]?.userNames.map((s: string) => (<Chip label={s} variant="outlined" />))}</TableCell>
+                    <TableCell align="right">{row.teamStats[1]?.userNames.map((s: string) => (<Chip label={s} variant="outlined" />))}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
